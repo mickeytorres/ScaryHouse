76d172db77f289cd11da56270af7e717a13619d2
Mickey Torres <met424@nyu.edu>
2019-10-06 14:46:07 -0400

    october 6 2019

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI; 
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Restart();
+        isPaused(); 
+    }
+
+    void isPaused()
+    {
+        if(Input.GetKey(KeyCode.Return)){
+            Time.timeScale = 0; 
+        }else{
+            Time.timeScale = 1; 
+        }
+    }
+
+    void Restart()
+    {
+        if(Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+        }
+    }
+}
